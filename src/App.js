26bee819ab9faab1bf2cf943a0aa69a962fee8a7
adityapsvs <adityapsvs@gmail.com>
2021-09-30 02:19:03 +0530

    Initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Card extends Component {
+  render() {
+    return (
+      <div className="card-wrapper">
+        card: {this.props.cardNum}
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.pageData = {
+      pageXData: [],
+      pageYData: []
+    }
+  }
+  captureMouseMoves = (event) => {
+    const pageXData = this.pageData.pageXData;
+    const pageYData = this.pageData.pageYData;
+    pageXData.push(event.pageX);
+    pageYData.push(event.pageY);
+    this.pageData = {
+      pageXData: pageXData,
+      pageYData: pageYData
+    }
+    console.log()
+  }
+  render() {
+    return (
+      <div className="App" onMouseMove={(event) => this.captureMouseMoves(event)}>
+        {
+          [1, 2, 3, 4, 5].map(x => <Card cardNum={x} />)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
